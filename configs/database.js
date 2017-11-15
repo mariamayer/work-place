bb02797a4b798e9ac6c3745de5890dccc94de69c
Maria Mayer <mayermaru@gmail.com>
2017-11-14 19:17:56 -0500

    Change db connection

--- configs/database.js
+++ configs/database.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect(`mongodb://localhost/demo`);
+mongoose.connect(`mongodb://heroku_qfx65dtc:vfisa2s10bc0ht66q8gmiu9800@ds149335.mlab.com:49335/heroku_qfx65dtc`);
 
 const db = mongoose.connection;
 
