b9198c0ed72f6b64c753fefc47cf8edcea4457ae
Maria Mayer <mayermaru@gmail.com>
2017-11-14 19:11:35 -0500

    Add filters

--- configs/database.js
+++ configs/database.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 // connect to the database
-mongoose.connect(`mongodb://heroku_qfx65dtc:vfisa2s10bc0ht66q8gmiu9800@ds149335.mlab.com:49335/heroku_qfx65dtc`);
+mongoose.connect(`mongodb://localhost/demo`);
 
 const db = mongoose.connection;
 
--- routes/addresses.js
+++ routes/addresses.js
@@ -5,17 +5,21 @@ const Address = require('../models/address-model');
 /* GET Addresses listing. */
 router.get('/addresses/:perPage/:page', (req, res, next) => {
   const sort = req.query.sortBy;
+  const filter = Number(req.query.filter);
   const criteria = req.query.criteria=='true' ? 'ascending' : 'descending';
   const perPage = Number(req.params.perPage);
   const page = req.params.page > 0 ? req.params.page : 0;
 
-  Address.count({}, function( err, count){
-    Address.find((err, addressesList) => {
+  Address.count({}, function(err, count){
+    Address.find(query(filter),(err, addressesList) => {
         if (err) {
           res.json(err); return;
         }
-        res.json(addressesList);
-      }).skip(perPage * (page-1)).limit(perPage).sort([[sort, criteria]]);
+        res.json({ "addresses": addressesList, "count": count});
+      })
+      .skip(perPage * (page-1))
+      .limit(perPage)
+      .sort([[sort, criteria]]);
     });
   });
 
@@ -31,3 +35,24 @@ router.get('/addresses/:perPage/:page', (req, res, next) => {
     });
 
 module.exports = router;
+
+// Add predefined filters
+const query= function(filter){
+  switch(filter) {
+    case 0:
+      return {};
+      break;
+    case 1:
+      return {LAST_SALE_PRICE: {$ne:''}};
+      break;
+    case 2:
+      return {LAST_SALE_PRICE: {$eq:''}};
+      break;
+    case 3:
+      return {OWNER_NAME_PRIMARY: {$ne:''}};
+      break;
+    case 4:
+      return {OWNER_NAME_PRIMARY: {$eq:''}};
+      break;
+  }
+}
